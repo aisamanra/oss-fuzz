5f375b1f7abdfe626fc826260be0a20f72469631
Getty Ritter <gettylefou@gmail.com>
2019-09-25 10:55:48 -0700

    Remove references to Clang toolchain in Sorbet build

--- projects/sorbet/build.sh
+++ projects/sorbet/build.sh
@@ -18,9 +18,10 @@
 FUZZERS=("fuzz_dash_e")
 PIPELINE_LIB=./bazel-bin/main/pipeline/libpipeline.a
 
-sed -i -e '/build --crosstool.*/d' .bazelrc
-sed -i -e '/build --copt=-Werror --copt=-Wimplicit-fallthrough/d' .bazelrc
-sed -i -e '5i#include <vector>' common/Subprocess.h
-sed -i -e '7i#include <cassert>' third_party/parser/include/ruby_parser/pool.hh
+# sed -i -e '/build --crosstool.*/d' .bazelrc
+# sed -i -e '/build --copt=-Werror --copt=-Wimplicit-fallthrough/d' .bazelrc
+# sed -i -e '5i#include <vector>' common/Subprocess.h
+# sed -i -e '7i#include <cassert>' third_party/parser/include/ruby_parser/pool.hh
+git apply sorbet.diff
 CXX="$CXX" CFLAGS="--std=c++17 --stdlib=libc++" CXXFLAGS="--std=c++17 --stdlib=libc++" bazel build --config=fuzz //test/fuzz:fuzz_dash_e
 cp bazel-bin/test/fuzz/fuzz_dash_e /out/.
